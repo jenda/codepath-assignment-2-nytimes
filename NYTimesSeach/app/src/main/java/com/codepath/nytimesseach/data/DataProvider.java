81516c817415fced500312f4f4d0579001a142d1
Jan Spidlen <jan.spidlen@airbnb.com>
2017-09-24 03:03:14 -0700

    Adding too-many-requests handling.

--- NYTimesSeach/app/src/main/java/com/codepath/nytimesseach/data/DataProvider.java
+++ NYTimesSeach/app/src/main/java/com/codepath/nytimesseach/data/DataProvider.java
@@ -6,12 +6,15 @@ import android.util.StringBuilderPrinter;
 import com.codepath.nytimesseach.model.Document;
 import com.codepath.nytimesseach.model.Response;
 import com.codepath.nytimesseach.settings.FilterSettings;
+import com.codepath.nytimesseach.utils.Constants;
 import com.google.common.collect.ImmutableSet;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -45,22 +48,16 @@ public class DataProvider {
     private final List<Document> dataDownloadedSoFar = new ArrayList<>();
     private final Set<String> idsDownloadedSoFar = new HashSet<>();
     private int currentPage = 0;
+    private Call<Response> call = null;
+    private boolean hasMore = true;
+    private long lastAttempt = System.currentTimeMillis();
 
     public DataProvider(DataFetchedListener dataFetchedListener) {
         this.dataFetchedListener = dataFetchedListener;
     }
 
-    public void fetchMoreInitial() {
-        fetchFresh(FilterSettings.getEmptyFilters());
-    }
-
-
-    public void fetchFresh(FilterSettings settings) {
-        clearBuffers();
-        fetchMore(0, settings);
-    }
-
     private void clearBuffers() {
+        hasMore = true;
         currentPage = 0;
         idsDownloadedSoFar.clear();
         dataDownloadedSoFar.clear();
@@ -81,16 +78,33 @@ public class DataProvider {
         return sb.toString();
     }
 
-    public synchronized void fetchMore(FilterSettings settings) {
+    public synchronized void fetchMoreInitial() {
+        fetchFresh(FilterSettings.getEmptyFilters());
+    }
+
+
+    public synchronized void fetchFresh(FilterSettings settings) {
+        clearBuffers();
+        fetchMore(0, null, settings);
+    }
+
+    public synchronized void fetchMore(String query, FilterSettings settings) {
         if (call != null && !call.isExecuted() && !call.isCanceled()) {
             Log.d("jenda", "returning");
             return;
         }
-        fetchMore(currentPage, settings);
+        fetchMore(currentPage, query, settings);
     }
-    Call<Response> call;
-    public synchronized void fetchMore(int page, FilterSettings settings) {
+    class Counter {
+        int counter;
+    }
+    public synchronized void fetchMore(int page, String query, FilterSettings settings) {
         // TODO: finish data and sorting.
+
+        if (!hasMore && lastAttempt > (System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(15))) {
+            Log.d("jenda", "too soon");
+            return;
+        }
 
         if (call != null && !call.isExecuted() && !call.isCanceled()) {
             Log.d("jenda", "returning");
@@ -101,12 +115,27 @@ public class DataProvider {
 
         Log.d("jenda:", "page " +  page + "");
         Log.d("jenda:", "fg " +  fg + "");
-//        Call<Response>
-                call = apiService.getDocs("android", page, API_KEY, fg);
 
+        final Counter retryCounter = new Counter();
+
+        call = apiService.getDocs(query, page, API_KEY, fg);
         call.enqueue(new Callback<Response>() {
             @Override
             public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+
+                if (response.code() == Constants.HTTP_TOO_MANY_REQUESTS) {
+                    Log.d("jenda:", " retrying ...");
+                    if (retryCounter.counter++ > 3) {
+                        throw new RuntimeException("too many retries");
+                    }
+                    try {
+                        Thread.currentThread().sleep(retryCounter.counter * 500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    call.clone().enqueue(this);
+                    return;
+                }
 
                 Log.d("jenda", "isSuccess: " + response.isSuccessful() + "");
                 Log.d("jenda", "code: " + response.code() + "");
@@ -114,13 +143,22 @@ public class DataProvider {
                 Log.d("jenda", response1.getStatus());
                 Log.d("jenda", "size: " + response1.getResponse().getDocs().size());
                 List<Document> docs = response1.getResponse().getDocs();
+
+                // Dedup and filter out known bad articles.
                 for(Document doc: docs) {
-                    if (idsDownloadedSoFar.add(doc.getId())) {
+                    if (!BLACK_LISTED_IDS.contains(doc.getId())
+                            && idsDownloadedSoFar.add(doc.getId())) {
                         dataDownloadedSoFar.add(doc);
                     }
                 }
 
-                currentPage++;
+                Log.d("jenda", "dataDownloadedSoFar  " + dataDownloadedSoFar.size());
+
+                lastAttempt = System.currentTimeMillis();
+                hasMore = docs.size() == 10;
+                if (hasMore) {
+                    currentPage++;
+                }
                 dataFetchedListener.onDataFetched(dataDownloadedSoFar);
             }
 
--- NYTimesSeach/app/src/main/java/com/codepath/nytimesseach/utils/Constants.java
+++ NYTimesSeach/app/src/main/java/com/codepath/nytimesseach/utils/Constants.java
@@ -6,4 +6,5 @@ package com.codepath.nytimesseach.utils;
 
 public class Constants {
     public static final String FILTERS = "filters";
+    public static final int HTTP_TOO_MANY_REQUESTS = 429;
 }
